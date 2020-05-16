c99ec5ca23a8cfaa2c49df6e76d2232b8c6f1a75
maekawatoshiki <konndennsa@gmail.com>
2020-05-17 02:37:18 +0900

    Works

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,16 @@
 extern crate minilisp;
 
-use std::io;
-
 fn main() {
     println!("Hello, world!");
 
-    let s = "   (+ 1 2)";
+    let s = "   (+ 1 (- 2 3))";
     println!("Before : {}", s);
 
-    let ss = minilisp::parser::tokenize(s);
-    for i in ss{
+    let mut ss = minilisp::parser::tokenize(s);
+    for i in &ss {
         println!("{}", i);
-    };
+    }
+
+    let x = minilisp::parser::read_from(&mut ss);
+    println!("latent expression: {:?}", x);
 }
