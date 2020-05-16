26c5ac53865b0483987c8bd697134904a159b822
Yusuke Kominami <yukke.konan@gmail.com>
2020-05-16 21:18:12 +0900

    added enum types

--- src/parser.rs
+++ src/parser.rs
@@ -1,7 +1,25 @@
+enum LispData {
+    Int(i32),
+    Float(f64),
+    Text(String),
+}
+
+
+enum LatentExpression {
+    Int(i32),
+    Float(f64),
+    Text(String),
+    List(Vec<LispData>)
+}
+
+
 pub fn tokenize(s: &str) -> Vec<String>{
     let spreaded = s.replace("(", " ( ").replace(")", " ) ");
     
-    let tokens: Vec<String> = a.split(" ").map(|item| item.to_string()).collect();
+    let tokens: Vec<String> = spreaded.split(" ").map(|item| item.to_string()).collect();
 
     return tokens;
 }
+
+
+//pub fn read_from(tokens: Vec<String>) 
