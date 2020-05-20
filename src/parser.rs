93b0d3222423daf7f500941e5f998cbf814958f9
Yusuke Kominami <yukke.konan@gmail.com>
2020-05-20 21:38:02 +0900

    removed unnecessary types

--- src/parser.rs
+++ src/parser.rs
@@ -1,28 +1,5 @@
 use std::collections::VecDeque;
 
-#[derive(Debug)]
-pub enum LatentExpression {
-    Int(i32),
-    Float(f64),
-    Text(String),
-    List(VecDeque<LatentExpression>),
-}
-
-
-impl LatentExpression {
-    pub fn as_vec(&mut self) -> &VecDeque<LatentExpression> {
-        match self {
-            LatentExpression::List(v) => v,
-            _ => panic!("Not List")
-        }
-    }
-
-    pub fn len(&mut self) -> usize {
-        self.as_vec().len()
-    }
-}
-
-
 pub fn tokenize(s: &str) -> VecDeque<String> {
     // Spread each tokens
     let spreaded = s.replace("(", " ( ").replace(")", " ) ");
@@ -35,37 +12,4 @@ pub fn tokenize(s: &str) -> VecDeque<String> {
         .collect();
 
     tokens
-}
-
-
-pub fn read_from(tokens: &mut VecDeque<String>) -> LatentExpression {
-    // List shouldn't be null
-    if tokens.len() == 0 {
-        panic!("Unexpected EOF while reading");
-    }
-
-    let token = tokens.pop_front().unwrap();
-
-    if token == "(" {
-        let mut list = VecDeque::new();
-        while tokens.len() > 0 && tokens[0] != ")" {
-            list.push_back(read_from(tokens))
-        }
-        return LatentExpression::List(list);
-    }
-
-    // Invalid parenthesis
-    if token == ")" {
-        panic!("Unexpected parenthesis");
-    }
-
-    if let Ok(value_int) = token.parse::<i32>() {
-        return LatentExpression::Int(value_int);
-    }
-
-    if let Ok(value_float) = token.parse::<f64>() {
-        return LatentExpression::Float(value_float);
-    }
-
-    LatentExpression::Text(token.to_string())
 }
